001f45f3097331b219615c713e4952ed4bd7db7f
fdurdu <hfdurdu@gmail.com>
2023-12-11 00:35:31 +0300

    added hello2

--- src/main/java/com/microservices/test/dockertestservice/controller/DockerTestController.java
+++ src/main/java/com/microservices/test/dockertestservice/controller/DockerTestController.java
@@ -22,6 +22,6 @@ public class DockerTestController {
     @GetMapping("/hello")
     public ResponseEntity<String> hello() {
 
-        return new ResponseEntity<>("Hello1", HttpStatus.OK);
+        return new ResponseEntity<>("Hello2", HttpStatus.OK);
     }
 }
